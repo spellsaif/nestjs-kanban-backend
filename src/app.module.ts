3c07307539c02eea934454f1613f4af4c68c7e44
Saif Ali <saif.ali067@gmail.com>
2022-09-28 03:11:51 +0530

    installing @prisma/client and setting up prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     envFilePath: '.env.development'
-  })],
+  }),
+
+    PrismaModule
+  ],
 
   controllers: [],
   providers: [],
